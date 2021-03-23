19b691f56ad6b98084866a017eaba6e06382fa7c
fighteros <am5419@fayoum.edu.eg>
2021-03-23 20:26:54 +0200

    fixes

---
+++ src/Chapter13/BB.java
@@ -0,0 +1,10 @@
+package Chapter13;
+
+public abstract class BB{
+    private int data;
+    private String name;
+    static double var;
+
+
+
+}
---
+++ src/Chapter13/Comparable.java
@@ -0,0 +1,20 @@
+package Chapter13;
+
+import java.awt.*;
+
+public class Comparable extends Rectangle implements java.lang.Comparable<ComparableRectangle> {
+    public ComparableRectangle(double width, double height) {
+        super(width, height);
+    }
+
+    @Override
+    public int compareTo(ComparableRectangle o) {
+        if (getArea() > o.getArea()) return 1;
+        else if (getArea() < o.getArea()) return -1;
+        else return 0;
+    }
+    @Override
+    public String toString(){
+        return super.toString()+ "Area" + getArea();
+    }
+}
